d9c131cd18dde5faadaa35b87e2f9d8824cf65f0
dadlaniGodel <delzydel@outlook.com>
2023-02-20 09:22:47 -0800

    Utilize the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: Alway return 0 if true
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
