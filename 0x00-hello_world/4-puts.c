891cca1d3a2cd7152dcdf3970bd35fc1c9e41af6
dadlaniGodel <delzydel@outlook.com>
2023-02-20 09:07:32 -0800

    Utilizing the put fucntion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - This is the main funtion
+ *
+ * Return: Always return zero 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return 0;
+}
