a89d301ef832078eae49f4301346896ba9e1bc20
dadlaniGodel <delzydel@outlook.com>
2023-02-20 19:42:07 -0800

    I play what I call the alphabet game mandatory

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Return 0 if true
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
