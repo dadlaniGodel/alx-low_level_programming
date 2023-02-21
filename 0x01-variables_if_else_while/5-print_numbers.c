7b86b0759b203452d82a8ce36aa2c6b07e9eaf11
dadlaniGodel <delzydel@outlook.com>
2023-02-20 20:28:22 -0800

    Nubers on the row

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Return 0 if true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
