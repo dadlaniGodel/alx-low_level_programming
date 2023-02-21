fb08ea8cc5931b828a841226b49777101de5c7e0
dadlaniGodel <delzydel@outlook.com>
2023-02-20 20:40:44 -0800

    Putchar on numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Returns 0 of true
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
